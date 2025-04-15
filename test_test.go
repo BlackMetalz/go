adadd7d1c818e4810ae8f537697d86830332a70d
Luong Trung Kien <kienlt.qn@gmail.com>
2025-04-16 00:06:06 +0700

    Learning, still

--- test_test.go
+++ test_test.go
@@ -1,15 +1,26 @@
 package main
 
 import (
-	"log"
 	"testing"
 )
 
-func TestAdd(t *testing.T) {
-	want := add(5, 5)
-	got := 10
+func TestGetUser(t *testing.T) {
+	md := &MockDataStore{
+		Users: map[int]User{
+			1: {ID: 1, First: "John"},
+		},
+	}
 
-	if want != got {
-		log.Fatalf("want %v, got %v", want, got)
+	s := &Service{
+		ds: md,
+	}
+
+	u ,err := s.GetUser(1)
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+
+	if u.First != "John" {
+		t.Fatalf("Expected John, got %s", u.First)
 	}
 }
